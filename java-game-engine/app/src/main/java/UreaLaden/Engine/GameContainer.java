3f4551b224a6e3d4b9e3e7eb571988f80644c914
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-04-03 17:48:05 -0400

    Created Game Loop Create Game Window

---
+++ java-game-engine/app/src/main/java/UreaLaden/Engine/GameContainer.java
@@ -0,0 +1,131 @@
+package UreaLaden.Engine;
+
+public class GameContainer implements Runnable
+{
+    private Thread thread;
+    private Window window;
+
+    private boolean running = false;
+    private final double UPDATE_CAP = 1.0 / 60.0; //Cap updates at 60 frames per second
+
+    private int width = 320, height= 240;
+    private float scale = 6f;
+    private String title = "UreaLaden.Engine v1.0";
+
+    public GameContainer()
+    {
+
+    }
+    public void start()
+    {
+        window = new Window(this);
+
+        thread = new Thread(this);
+        thread.run(); //.run is for main thread
+    }
+    public void stop(){
+
+    }
+    public void run()
+    {
+        running = true;
+        boolean render = false;
+        double firstTime = 0;
+        double lastTime = System.nanoTime() / 1000000000.0;
+        double passedTime = 0;
+        double unprocessedTime = 0;
+
+        double frameTime = 0;
+        int frames = 0; //count passed frames
+        int fps = 0; //frames per second
+
+        while(running)
+        {
+            render = false;
+            //Get current time and determine how long it has been
+            firstTime = System.nanoTime() / 1000000000.0;
+            passedTime = firstTime - lastTime;
+            lastTime = firstTime;
+
+            unprocessedTime += passedTime;
+            frameTime += passedTime;
+
+            while(unprocessedTime >=UPDATE_CAP)
+            {
+                //Updates twice if frames freeze
+                unprocessedTime -= UPDATE_CAP;
+                render = true; // only when we update
+                //TODO: Update game
+                if(frameTime >= 1.0)
+                {
+                    frameTime = 0;
+                    fps = frames;
+                    frames = 0;
+                    System.out.println("FPS: " + fps);
+                }
+            }
+
+            if(render)
+            {
+                //TODO: Render game
+                window.update();
+                frames++;
+
+            }
+            else
+            {
+                try
+                {
+                    Thread.sleep(1); //If not updating or rendering don't do anything. Takes it
+                    // easy on CPU
+                }
+                catch(InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        dispose();
+    }
+    private void dispose()
+    {
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public static void main(String[] args){
+        GameContainer gc = new GameContainer();
+        gc.start();
+    }
+}
---
+++ java-game-engine/app/src/main/java/UreaLaden/Engine/Window.java
@@ -0,0 +1,45 @@
+package UreaLaden.Engine;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+public class Window
+{
+    private JFrame frame;
+    private BufferedImage image; //Store pixel data for screen
+    private Canvas canvas;
+    private BufferStrategy bs;
+    private Graphics g;
+
+    public Window(GameContainer gc)
+    {
+        image  = new BufferedImage(gc.getWidth(),gc.getHeight(),BufferedImage.TYPE_INT_RGB); //image stored in ram
+        canvas = new Canvas();
+        Dimension s = new Dimension((int)(gc.getWidth() * gc.getScale()),
+                (int)(gc.getHeight()*gc.getScale()));
+        canvas.setPreferredSize(s);
+        canvas.setMaximumSize(s);
+        canvas.setMinimumSize(s);
+
+        frame = new JFrame(gc.getTitle());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //program ends on clicking x button
+        frame.setLayout(new BorderLayout()); // matches canvas to border
+        frame.add(canvas,BorderLayout.CENTER);
+        frame.pack(); //sets frame to size of the canvas
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        canvas.createBufferStrategy(2);
+        bs = canvas.getBufferStrategy();
+        g = bs.getDrawGraphics();
+    }
+
+    public void update()
+    {
+        g.drawImage(image,0,0,canvas.getWidth(), canvas.getHeight(),null);
+        bs.show();
+    }
+}
